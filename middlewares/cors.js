7be1a60031e664347a9a2a1fe3fc02eb06f16903
ovaqvado <likeriklohhg@gmail.com>
2024-05-25 00:52:21 +0300

    save new settings

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,6 @@
 const allowedCors = ["https://frontendgames-yp.nomoredomainswork.ru", "http://localhost:3000", "http://localhost:3001"];
 
+
 function cors(req, res, next) {
     const { origin } = req.headers;
 
@@ -7,10 +8,7 @@ function cors(req, res, next) {
         res.header("Access-Control-Allow-Origin", origin);
     }
     res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
-    );
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next();
 }
 
